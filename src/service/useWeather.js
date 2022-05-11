95af999bf54ac70086e3cea4e4ee3d7a509c172f
Gabriel Rea <gabriel.rea96@gmail.com>
2022-05-11 12:27:38 -0300

    fix CORS with Heroku

--- src/service/useWeather.js
+++ src/service/useWeather.js
@@ -1,18 +1,27 @@
 import axios from "axios";
 
+const CROSS_DOMAIN = "https://mycorsproxy-crossdomainyz.herokuapp.com/";
+const API_URL = "https://www.metaweather.com/api/location/";
+
+const api = axios.create({
+  baseURL: `${CROSS_DOMAIN}${API_URL}`,
+  headers: {
+    "Access-Control-Allow-Headers": "X-Requested-With",
+    "X-Requested-With": "XMLHttpRequest",
+  },
+});
+
 export const useWeather = () => {
   const getWeather = async (woeid) => {
-    const res = await axios.get(
-      `https://www.metaweather.com/api/location/${woeid}`
-    );
+    const res = await api.get(`${woeid}`);
     const data = await res.data;
 
     return data;
   };
 
   const getLocation = async (location) => {
-    const res = await axios.get(
-      `https://www.metaweather.com/api/location/search/?lattlong=${location.latitude},${location.longitude}`
+    const res = await api.get(
+      `search/?lattlong=${location.latitude},${location.longitude}`
     );
     const data = await res.data;
 
@@ -20,9 +29,7 @@ export const useWeather = () => {
   };
 
   const getSearch = async (search) => {
-    const res = await axios.get(
-      `https://www.metaweather.com/api/location/search?query=${search}`
-    );
+    const res = await api.get(`search?query=${search}`);
     const data = await res.data;
 
     return data;
